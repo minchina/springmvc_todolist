e5dadfb9f1609f8b2a0a267b65b334bf7bd7d032
minchina <minchina91@gmail.com>
2015-01-25 11:39:12 +0800

    add query

--- src/main/java/com/tw/todolist/repositoy/ToDoRepository.java
+++ src/main/java/com/tw/todolist/repositoy/ToDoRepository.java
@@ -2,6 +2,8 @@ package com.tw.todolist.repositoy;
 
 import com.tw.todolist.Domain.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,5 +16,8 @@ public interface ToDoRepository extends JpaRepository<ToDo, Long>{
 
     List<ToDo> findByUserId(Long userId);
 
+    @Modifying
+    @Transactional
+    @Query("delete from ToDo todo where todo.id = ?1")
     void deleteById(Long id);
 }
