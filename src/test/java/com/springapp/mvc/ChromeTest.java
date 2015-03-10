000b7fc820c8b9b7853d5b026775185b00550613
minchina <minchina91@gmail.com>
2015-03-10 10:22:51 +0800

    do some refactor

--- src/test/java/com/springapp/mvc/ChromeTest.java
+++ src/test/java/com/springapp/mvc/ChromeTest.java
@@ -13,32 +13,35 @@ import static org.springframework.test.util.MatcherAssertionErrors.assertThat;
 
 public class ChromeTest {
 
-    public static String chromeDriverPath = "/home/minchina/program/chromedriver";
-    public static String chromeDriver = "webdriver.chrome.driver";
-
+    public static String CHROMEDRIVERPATH = "/home/minchina/program/chromedriver";
+    public static String CHROMEDRIVER = "webdriver.chrome.driver";
+    private String url = "http://www.baidu.com";
+    final String searchKey = "成都信息工程学院";
     @Test
     public void should_search_and_go_to_result_page_success(){
-        final String searchKey = "成都信息工程学院";
-        System.setProperty(chromeDriver, chromeDriverPath);
+        initBrowserPath(CHROMEDRIVER, CHROMEDRIVERPATH);
+
         WebDriver driver = new ChromeDriver();
-        driver.get("http://www.baidu.com");
+
+        driver.get(url);
         String firstPageTitle = driver.getTitle();
         assertThat(firstPageTitle,is("百度一下，你就知道"));
         
         WebElement element = driver.findElement(By.id("kw"));
         element.sendKeys(searchKey);
         element.submit();
+
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
                 return d.getTitle().toLowerCase().substring(0, searchKey.length()).endsWith(searchKey);
             }
         });
+
         String secondPageTitle = driver.getTitle();
         assertThat(secondPageTitle.substring(0, searchKey.length()), is(searchKey));
         driver.quit();
     }
-
-    private void print(String context){
-        System.out.println(context);
+    private void initBrowserPath(String driver, String driverPath){
+        System.setProperty(driver, driverPath);
     }
 }
