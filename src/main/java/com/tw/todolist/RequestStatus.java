86d674c39c05dbdc5d662d6cbfb5ca3426bcc6a4
minchina <minchina91@gmail.com>
2015-03-19 15:52:47 +0800

    move resoures out of java

---
+++ src/main/java/com/tw/todolist/RequestStatus.java
@@ -0,0 +1,11 @@
+package com.tw.todolist;
+
+public enum RequestStatus {
+    MON,
+    TUE,
+    WED,
+    THU,
+    FRI,
+    SAT,
+    SUN
+}
--- src/test/java/com/springapp/mvc/BddTest.java
+++ src/test/java/com/springapp/mvc/BddTest.java
@@ -2,6 +2,7 @@ package com.springapp.mvc;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,27 +10,30 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.hamcrest.core.Is.is;
-import static org.springframework.test.util.MatcherAssertionErrors.assertThat;
+import static org.junit.Assert.assertThat;
 
-public class ChromeTest {
+public class BddTest {
+
 
     public static String CHROMEDRIVERPATH = "/home/minchina/program/chromedriver";
     public static String CHROMEDRIVER = "webdriver.chrome.driver";
-    private String url = "http://www.baidu.com";
+    public static String url = "http://www.baidu.com";
     final String searchKey = "成都信息工程学院";
 
     @Test
-    public void should_search_and_go_to_result_page_success() {
+    public void should_(){
         initBrowserPath(CHROMEDRIVER, CHROMEDRIVERPATH);
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get(url);
+        driver.navigate().to(url);
+//        driver.get(url);
         String firstPageTitle = driver.getTitle();
-        assertThat(firstPageTitle, is("百度一下，你就知道"));
-
+        assertThat(firstPageTitle,is("百度一下，你就知道"));
+        ((JavascriptExecutor)driver).executeScript("alert(\'hehe\')");
         WebElement element = driver.findElement(By.id("kw"));
         element.sendKeys(searchKey);
+
         element.submit();
 
         (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
@@ -39,12 +43,13 @@ public class ChromeTest {
         });
 
         String secondPageTitle = driver.getTitle();
+        System.out.println(driver.getCurrentUrl());
         assertThat(secondPageTitle.substring(0, searchKey.length()), is(searchKey));
         driver.quit();
+
     }
 
-    private void initBrowserPath(String driver, String driverPath) {
+    private void initBrowserPath(String driver, String driverPath){
         System.setProperty(driver, driverPath);
     }
-
 }
