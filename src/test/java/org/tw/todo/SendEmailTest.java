cf130c8bdc5ce64686ebebd3070c83fb53f05eb5
minchina <minchina91@gmail.com>
2015-09-09 22:55:53 +0800

    test chinese testing name

--- src/test/java/org/tw/todo/SendEmailTest.java
+++ src/test/java/org/tw/todo/SendEmailTest.java
@@ -2,8 +2,9 @@ package org.tw.todo;
 
 import com.tw.todolist.domain.Customer;
 import com.tw.todolist.domain.Order;
-import com.tw.todolist.service.impl.RegistrationService;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class SendEmailTest {
 
@@ -11,5 +12,11 @@ public class SendEmailTest {
     public void shouldThrowException() {
         Customer customer = new Customer("mao", "nc", "minchina91@gmail.com");
         Order order = new Order(customer, "1234");
+    }
+
+    @Test
+    public void 仅仅只是为了测试中文 () {
+        Boolean True = Boolean.TRUE;
+        assertTrue(True);
     }
 }
