d5bd192fd2a2fad986714611c56f9f6271b17f67
minchina <minchina91@gmail.com>
2015-09-22 22:15:54 +0800

    refactor functional test

--- src/test/java/org/tw/todo/functional/HelloWorldTest.java
+++ src/test/java/org/tw/todo/functional/HelloWorldTest.java
@@ -1,6 +1,7 @@
 package org.tw.todo.functional;
 
 import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.tw.todo.pages.LoginPage;
@@ -9,14 +10,14 @@ import org.tw.todo.pages.TestFixure;
 @RunWith(ConcordionRunner.class)
 public class HelloWorldTest  {
 
-    private LoginPage loginPage;
-    private WebDriver webDriver;
+    public static LoginPage loginPage;
+    public static WebDriver webDriver;
 
-    public HelloWorldTest() {
+    @BeforeClass
+    public static void setUp() {
         webDriver = TestFixure.getWebDriver();
         loginPage = new LoginPage(webDriver);
         loginPage.open();
-
     }
 
     public void login(String userName, String password) {
