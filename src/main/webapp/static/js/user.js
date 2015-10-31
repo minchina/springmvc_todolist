bcf819ebf69f23b18cbcbea078d2faaea97211a2
minchina <minchina91@gmail.com>
2015-10-31 22:04:23 +0800

    refactor user.js

--- src/main/webapp/static/js/user.js
+++ src/main/webapp/static/js/user.js
@@ -1,29 +1,20 @@
 $(document).ready(function(){
 
+    var add_user_url = "/users/add";
+    var delete_user_url = "/users/delete";
+
    var $name_input = $('#new-todo');
    $name_input.keydown(function(e){
       if(e.keyCode==13){
          var user_name = $name_input.val();
-         $.ajax({
-            url:"/users/add",
-            data:{user_name:user_name},
-            type:"POST",
-            success:addUserSuccessCallback
-         })
+          httpService(add_user_url,{user_name:user_name}, addUserSuccessCallback);
       }
    });
 
     $(".destroy").on("click",function(){
         var $li = $(this).closest("li");
         var user_id = $li.data("id");
-        $.ajax({
-            url:"/users/delete",
-            data:{user_id:user_id},
-            type:"POST",
-            success:function(){
-                $li.remove();
-            }
-        })
+        httpService(delete_user_url, {user_id:user_id}, deleteUserCallback($li));
     });
 
     function addUserSuccessCallback(json_user){
@@ -31,6 +22,10 @@ $(document).ready(function(){
         var $user_list = $("#todo-list");
         $user_list.append(concatString(user.name,user.id));
         $name_input.val("");
+    }
+
+    function deleteUserCallback (event) {
+        event.remove();
     }
 
    function concatString(user_name,user_id){
@@ -43,4 +38,13 @@ $(document).ready(function(){
         "</li>";
       return expectString;
    }
+
+    function httpService (url,data,callback) {
+        $.ajax({
+            url:url,
+            data:data,
+            type:"POST",
+            success:callback
+        })
+    }
 });
