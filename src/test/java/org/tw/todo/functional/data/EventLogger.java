58b5502d92c21c492b875cc488b0500001c2a777
minchina <minchina91@gmail.com>
2015-11-24 13:30:25 +0800

    add a log class

---
+++ src/test/java/org/tw/todo/functional/data/EventLogger.java
@@ -0,0 +1,41 @@
+package org.tw.todo.functional.data;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.Maps;
+import org.apache.commons.lang.ObjectUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+
+public class EventLogger {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventLogger.class);
+
+    public EventLogger() {
+    }
+
+
+    public static void log(String eventName, String key,Object value) {
+        Map<String, Object> message = Maps.newLinkedHashMap();
+        message.put(key, value);
+        log(eventName, message);
+    }
+
+
+    public static void log(String eventName, Map<String, Object> message) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode nodes = mapper.createObjectNode();
+        nodes.put("event", eventName);
+
+        for(String key: message.keySet()) {
+            nodes.put(key, ObjectUtils.toString(message.get(key), "Undefined"));
+        }
+
+        LOGGER.info(nodes.toString());
+    }
+
+
+}
