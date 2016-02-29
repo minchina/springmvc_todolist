eebe8e93f430710557a55af31090e25467e02c2a
minchina <minchina91@gmail.com>
2016-02-29 16:44:59 +0800

    refactor mybatis to use annotation

--- src/main/java/com/tw/todolist/mappers/UserMapper.java
+++ src/main/java/com/tw/todolist/mappers/UserMapper.java
@@ -1,8 +1,12 @@
 package com.tw.todolist.mappers;
 
 import com.tw.todolist.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 public interface UserMapper {
 
+    String SELECT_USER = "select * from user where id = #{id}";
+
+    @Select(SELECT_USER)
     User selectUser(Long id);
 }
